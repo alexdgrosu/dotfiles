2285de9017ec0e2fee32b94ab6a7d98eb72c9671
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-02-11 14:41:41 +0100

    feat(zsh): Add named 02.directories.zsh rc.d config

---
+++ config/zsh/rc.d/02.directories.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+###
+# Named directories
+#
+
+# Create shortcuts for common directories.
+# `hash -d <name>=<path>` makes ~<name> a shortcut for <path>.
+hash -d config="${XDG_CONFIG_HOME}"
+hash -d   asdf="${ASDF_DIR}"
+hash -d    dot="${HOME}/.dotfiles"
+hash -d    ssh="${HOME}/.ssh"
+hash -d    zsh="${ZDOTDIR}"
+hash -d   temp="${HOME}/temp"
+hash -d    src="${HOME}/sources"
