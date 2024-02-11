c61c88f89c862215e719be46b308b48031a2e4b4
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-02-11 14:50:14 +0100

    feat(zsh): Add asdf & 03.environment.zsh rc.d config with global functions

---
+++ config/zsh/rc.d/03.environment.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+###
+# Environment variables
+#
+
+bindkey -e
+
+# -U ensures each entry is unique
+export -U PATH path FPATH fpath MANPATH manpath
+export -UT INFOPATH infopath
+
+# Each value in the array is expanded separately
+path=(
+    $path
+    "~/.local/bin"
+)
+
+fpath=(
+    "${XDG_CONFIG_HOME}/functions"
+    $fpath
+)
+
+# We source asdf early to make sure its shims are available in the path
+source "$ASDF_DIR/asdf.sh"
