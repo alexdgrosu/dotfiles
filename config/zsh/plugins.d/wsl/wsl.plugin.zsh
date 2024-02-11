52412b197f3de7b2709b16520ac0db85ff600c85
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-02-11 15:07:24 +0100

    feat(zsh): Add antidote 05.plugins.zsh rc.d config

---
+++ config/zsh/plugins.d/wsl/wsl.plugin.zsh
@@ -0,0 +1,2 @@
+alias c="/mnt/c/WINDOWS/system32/clip.exe"
+alias o="/mnt/c/WINDOWS/explorer.exe ."
---
+++ config/zsh/rc.d/05.plugins.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+###
+# Plugin manager
+#
+# Plugins are configured in /zsh/zsh_plugins.txt
+#
+
+autoload -U $XDG_CONFIG_HOME/functions/*
+
+source "$ANTIDOTE_DIR/antidote.zsh"
+antidote load
+
+# We initialize bash-completion because certain plugins rely on it.
+autoload -Uz bashcompinit; bashcompinit
+
+# Hook direnv so it can automatically load .envrc when cd-ing to a directory.
+eval "$(direnv hook zsh)"
