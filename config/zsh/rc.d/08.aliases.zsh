24fffbd8b6cb75b06680c2b45a39cad8ec63a26c
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-02-11 15:22:24 +0100

    feat(zsh): Add 08.aliases.zsh rc.d config

---
+++ config/zsh/rc.d/08.aliases.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+###
+# Comands, functions, aliases
+#
+
+setopt AUTO_CD
+
+alias    ..='cd ..'
+alias   ...='cd ../../'
+alias  ....='cd ../../../'
+alias .....='cd ../../../../'
+
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias cat='bat'
+alias eza='eza --icons --group-directories-first'
+alias  ls='eza'
+alias   g='git'
