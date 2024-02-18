c695c0c9a98e2d48f8db331d404dd1e6072315e0
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-02-18 19:20:32 +0100

    fix: bashcompinit before antidote in case plugins need it

--- config/zsh/rc.d/05.plugins.zsh
+++ config/zsh/rc.d/05.plugins.zsh
@@ -9,11 +9,11 @@
 
 autoload -U $XDG_CONFIG_HOME/functions/*
 
-source "$ANTIDOTE_DIR/antidote.zsh"
-antidote load
-
 # We initialize bash-completion because certain plugins rely on it.
 autoload -Uz bashcompinit; bashcompinit
+
+source "$ANTIDOTE_DIR/antidote.zsh"
+antidote load
 
 # Hook direnv so it can automatically load .envrc when cd-ing to a directory.
 eval "$(direnv hook zsh)"
