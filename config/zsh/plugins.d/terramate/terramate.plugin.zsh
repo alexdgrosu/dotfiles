e405f254df11afb8256990337aee481e8c0eefe6
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-02-27 16:16:39 +0100

    feat(plugins.d): Add tm wrapper for terramate

--- config/zsh/plugins.d/terramate/terramate.plugin.zsh
+++ config/zsh/plugins.d/terramate/terramate.plugin.zsh
@@ -1,1 +1,19 @@
-complete -o nospace -C /home/adg/.asdf/installs/terramate/0.4.3/bin/terramate terramate 
+function tm() {
+    terramate fmt
+
+    case "$1" in
+
+    "-watch-fmt")
+        echo "🤖 Watching .hcl files to run fmt..."
+        watchexec -r -e 'hcl' 'terramate fmt'
+        ;;
+    "")
+        terramate generate
+        ;;
+    *)
+        terramate "$@"
+        ;;
+    esac
+}
+
+complete -o nospace -C /home/adg/.asdf/installs/terramate/0.4.3/bin/terramate terramate tm
