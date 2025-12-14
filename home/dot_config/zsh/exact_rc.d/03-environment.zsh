a8cc6a704edd57d60299bae604baec4441201e36
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2025-12-14 08:22:52 +0100

    feat: add ~/.config/zsh/rc.d/03-environment.zsh

---
+++ home/dot_config/zsh/exact_rc.d/03-environment.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+# ---------------------------------------------------------------------------- #
+#                             Environment Variables                            #
+# ---------------------------------------------------------------------------- #
+
+bindkey -e
+
+# -U ensures each entry is unique
+export -U PATH path FPATH fpath MANPATH manpath
+export -UT INFOPATH infopath
+
+# Each value in the array is expanded separately
+path=(
+    $path
+    ~/.local/bin
+)
+
+fpath=(
+    "${XDG_CONFIG_HOME}/functions"
+    $fpath
+)
