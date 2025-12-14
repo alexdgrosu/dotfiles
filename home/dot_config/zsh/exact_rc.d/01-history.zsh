f01c38eceebfabd5f2bce6b4c111258b32110aea
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2025-12-14 08:21:56 +0100

    feat: add ~/.config/zsh/rc.d/01-history.zsh

---
+++ home/dot_config/zsh/exact_rc.d/01-history.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+##
+# History settings
+#
+# We set all history options early to preserve history no matter what happens.
+#
+
+# https://zsh.sourceforge.io/Doc/Release/Expansion.html#Filename-Generation
+setopt EXTENDED_GLOB
+
+HISTFILE=${XDG_DATA_HOME:=-~/.local/share}/zsh/history
+
+# Ensure the directory structure for the history file exists.
+[[ -d $HISTFILE:h ]] || mkdir -p $HISTFILE:h
+
+# Max number of entries to keep in the history file.
+SAVEHIST=$((100 * 1000))
+
+# Max number of entries to keep in memory.
+HISTSIZE=$((1.2 * SAVEHIST))
+
+# Use modern file-locking mechanisms.
+setopt HIST_FCNTL_LOCK
+
+# Remove duplicates from history.
+setopt HIST_IGNORE_ALL_DUPS
+
+# Sync history between sessions.
+setopt SHARE_HISTORY
+
+# Do not save lines that start with a ` ` to history.
+setopt HIST_IGNORE_SPACE
