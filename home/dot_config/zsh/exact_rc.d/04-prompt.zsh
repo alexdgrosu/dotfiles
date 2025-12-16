4b3195e63569802b0f46b75c5769b4c94ade2769
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2025-12-16 07:26:24 +0100

    feat: add ~/.config/zsh/rc.d/04-prompt.zsh

---
+++ home/dot_config/zsh/exact_rc.d/04-prompt.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+setopt PROMPT_SUBST
+unsetopt BEEP
+unsetopt CORRECT
+
+export FZF_DEFAULT_COMMAND="fdfind --strip-cwd-prefix --follow"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
+
+# Configure fzf (history, file completion)
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+  --color=prompt:#32344a,pointer:#787c99,marker:#acb0d0,spinner:#acb0d0,header:#414868
+  --border='sharp' --layout=reverse
+  --prompt='❯❯ ' --marker='✱' --pointer='❯'
+  --height=99%
+  --bind 'ctrl-/:toggle-preview'
+  --preview='batcat --color=always {}' --preview-window 'down:75%:hidden'"
+
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window 'up:3:hidden:wrap'"
+
+export FZF_ALT_C_OPTS="
+  --preview 'eza -T {} | head -200' --preview-window 'down:50%:hidden'"
+
+source <(fzf --zsh)
+
+# Initialize starship (prompt)
+export STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship/config.toml"
+export STARSHIP_CACHE="${XDG_CACHE_HOME}/starship"
+eval "$(starship init zsh)"
