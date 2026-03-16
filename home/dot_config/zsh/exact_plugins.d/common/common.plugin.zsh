3f124a3ad1708648c4a4612e19fa0bac47481c80
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2026-03-16 10:58:51 +0100

    refactor(zsh): replace agkozak/zsh-z with zoxide

--- home/dot_config/zsh/exact_plugins.d/common/common.plugin.zsh
+++ home/dot_config/zsh/exact_plugins.d/common/common.plugin.zsh
@@ -1,3 +1,6 @@
+# https://github.com/ajeetdsouza/zoxide/issues/491#issuecomment-2137626085
+eval "$(zoxide init zsh | sed 's/_files/_cd/g')"
+
 compdef _gnu_generic fzf
 compdef _gnu_generic fdfind
 
